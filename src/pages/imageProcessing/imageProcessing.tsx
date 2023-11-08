a4444a2513fd85e62d34ac7014699d9cc0b50460
TomSeparovich <135411478+TomSeparovich@users.noreply.github.com>
2023-11-09 01:21:28 +1100

    Minor changes to button within imageProcessing

--- src/pages/imageProcessing/imageProcessing.tsx
+++ src/pages/imageProcessing/imageProcessing.tsx
@@ -7,17 +7,30 @@ const ImageProcessing: React.FC = () => {
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { files } = event.target;
         if(files) setSelectedFiles(files);
-      };
+    };
+    
+    //Should add preventitive measure to stop button being pressed again
+    const processFiles = async() => {
+        const files = selectedFiles; //This will allow a user to select files for next upload without affect the current
+        if(!files) return; //Ensure there are selected files
+
+        //Call face loading
+
+        for(const file of files){
+
+        }
+    };
+
+    
 
     return (
         <div>
             <div>
                 <h2>Select Files</h2>
                 <input type="file" onChange={handleFileChange} multiple />
-                <button type="submit">Process</button>
+                <button onClick={processFiles}>Process</button>
                 <p>*Should work for file types JPEG, PNG, GIF, BMP, WebP, SVG</p>
                 <p>**Other types will need additional testing i.e heic</p>
-                <p>***This button does nothing currently</p>
             </div>
             <div>
                 <h2>Selected Files:</h2>
