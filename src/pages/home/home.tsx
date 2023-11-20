dd7022c1e64092f6a06b2f032c2f5dab2764eaf7
TomSeparovich <135411478+TomSeparovich@users.noreply.github.com>
2023-11-20 13:14:28 +1100

    Installed faceid-api.js
    Still awaiting models

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,25 +1,37 @@
 import { Link, Outlet } from 'react-router-dom';
 import './home.css';
 
-const Home: React.FC = () => {
+import { loadModals } from '../../services/faceid';
+import { useEffect, useState } from 'react';
 
-    return (
-        <>
-          <div id="sidebar">
-            <h2>Navigation</h2>
-            <nav>
-              <ul>
-                <li><Link to={`imageprocessing`}>Image Processing</Link></li>
-                <li><Link to={`dbmanage`}>Database Management</Link></li>
-                <li><Link to={''}>Home</Link></li>
-              </ul>
-            </nav>
-          </div>
-          <div id="main">
-            <Outlet />
-          </div>
-        </>
-      );
+const Home: React.FC = () => {
+  const [modelStatus, setModelStatus] = useState<string>('Models Loading');
+
+  useEffect(() => {
+    loadModals().then(() => {
+      setModelStatus('Modals Loaded');
+    });
+  }, []);
+
+
+  return (
+    <>
+      <div id="sidebar">
+        <h2>Navigation</h2>
+        <nav>
+          <ul>
+            <li><Link to={`imageprocessing`}>Image Processing</Link></li>
+            <li><Link to={`dbmanage`}>Database Management</Link></li>
+            <li><Link to={''}>Home</Link></li>
+          </ul>
+        </nav>
+        <p>{modelStatus}</p>
+      </div>
+      <div id="main">
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default Home;
